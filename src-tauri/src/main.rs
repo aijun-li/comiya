40d1e9f95f563622a4cd15814067d970940c04ff
liaijun <liaijun.cn@gmail.com>
2024-02-29 23:18:54 +0800

    feat: demo

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use scraper::{Html, Selector};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn greet() -> String {
+    let html = reqwest::get("https://www.manhuagui.com/s/怪兽.html")
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    let document = Html::parse_document(&html);
+    let selector = Selector::parse(".book-result ul > .cf .book-detail > dl > dt > a").unwrap();
+
+    let title = document
+        .select(&selector)
+        .next()
+        .unwrap()
+        .attr("title")
+        .unwrap();
+
+    title.to_string()
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
