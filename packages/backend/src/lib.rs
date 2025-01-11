ee574a6ea1834120a16235e494608c7b94d18355
liaijun <liaijun.cn@gmail.com>
2025-01-11 16:31:05 +0800

    feat: mhg search

--- packages/backend/src/lib.rs
+++ packages/backend/src/lib.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use dom_query::Document;
+use dom_query::{Document, Selection};
 
 #[derive(Debug)]
 pub struct ComicBrief {
@@ -10,8 +10,10 @@ pub struct ComicBrief {
     pub pub_date: String,
 }
 
+#[trait_variant::make(Send)]
 pub trait Site {
     async fn get_comic_brief(&self, id: String) -> Result<ComicBrief>;
+    async fn search_comic(&self, keyword: String) -> Result<Vec<ComicBrief>>;
 }
 
 pub struct Manhuagui;
@@ -24,18 +26,95 @@ impl Site for Manhuagui {
             .await?;
 
         let doc = Document::from(body);
-        let name = doc
-            .select_single(".book-detail>.book-title>h1")
-            .text()
-            .to_string();
-        let intro = doc.select_single("#intro-cut").text().to_string();
 
-        Ok(ComicBrief {
-            name,
-            cover: "".to_string(),
-            author: vec![],
-            intro,
-            pub_date: "".to_string(),
-        })
+        let brief = parse_brief(
+            &doc.select_single("body"),
+            &BriefSelectors {
+                cover: ".book-cover>.hcover>img",
+                name: ".book-detail>.book-title>h1",
+                author: ".book-detail>.detail-list>li:nth-child(2)>span:nth-child(2)>a",
+                pub_date: ".book-detail>.detail-list>li:first-child>span:first-child>a",
+                intro: "#intro-cut",
+            },
+        );
+
+        Ok(brief)
+    }
+
+    async fn search_comic(&self, keyword: String) -> Result<Vec<ComicBrief>> {
+        let encoded: String = url::form_urlencoded::byte_serialize(keyword.as_bytes()).collect();
+        let body = reqwest::get(format!("https://www.manhuagui.com/s/{}.html", encoded))
+            .await?
+            .text()
+            .await?;
+
+        let doc = Document::from(body);
+
+        let selectors = BriefSelectors {
+            cover: ".book-cover>.bcover>img",
+            name: ".book-detail dt>a",
+            author: ".book-detail dd.tags:nth-of-type(3)>span>a",
+            pub_date: ".book-detail dd.tags:nth-of-type(2)>span:first-child>a",
+            intro: ".book-detail dd.intro>span",
+        };
+
+        let list = doc
+            .select(".book-result>ul>li.cf")
+            .iter()
+            .map(|item| parse_brief(&item, &selectors))
+            .collect();
+
+        Ok(list)
+    }
+}
+
+struct BriefSelectors<'a> {
+    cover: &'a str,
+    name: &'a str,
+    author: &'a str,
+    pub_date: &'a str,
+    intro: &'a str,
+}
+
+fn parse_brief(node: &Selection, selectors: &BriefSelectors) -> ComicBrief {
+    let name = node
+        .select_single(&selectors.name)
+        .text()
+        .trim()
+        .to_string();
+
+    let mut cover = node
+        .select_single(&selectors.cover)
+        .text()
+        .trim()
+        .to_string();
+    if cover.starts_with("//") {
+        cover = format!("https:{cover}");
+    }
+
+    let author: Vec<String> = node
+        .select(&selectors.author)
+        .iter()
+        .map(|item| item.text().trim().to_string())
+        .collect();
+
+    let pub_date = node
+        .select_single(&selectors.pub_date)
+        .text()
+        .trim()
+        .to_string();
+
+    let intro = node
+        .select_single(&selectors.intro)
+        .text()
+        .trim()
+        .to_string();
+
+    ComicBrief {
+        name,
+        cover,
+        author,
+        pub_date,
+        intro,
     }
 }
--- packages/backend/src/main.rs
+++ packages/backend/src/main.rs
@@ -1,9 +1,16 @@
 use backend::{Manhuagui, Site};
 
 async fn test<T: Site>(site: T) {
-    match site.get_comic_brief("7580".to_string()).await {
-        Ok(brief) => {
-            println!("{:?}", brief);
+    // match site.get_comic_brief("7580".to_string()).await {
+    //     Ok(brief) => {
+    //         println!("{:?}", brief);
+    //     }
+    //     _ => return,
+    // }
+
+    match site.search_comic("一拳".to_string()).await {
+        Ok(list) => {
+            println!("{:?}", list);
         }
         _ => return,
     }
