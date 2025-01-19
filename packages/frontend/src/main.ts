5183c6f3d2be7f766926486c834af8958e19fd71
liaijun <liaijun.cn@gmail.com>
2025-01-19 23:37:03 +0800

    feat: support pwa

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -1,9 +1,12 @@
 import { VueQueryPlugin } from '@tanstack/vue-query';
 import { createPinia } from 'pinia';
+import { registerSW } from 'virtual:pwa-register';
 import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router';
 import './style.css';
+
+registerSW({ immediate: true });
 
 const pinia = createPinia();
 
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,10 +2,62 @@ import vue from '@vitejs/plugin-vue';
 import vueRouter from 'unplugin-vue-router/vite';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vueRouter(), vue()],
+  plugins: [
+    vueRouter(),
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'inline',
+      manifest: {
+        name: 'Comiya',
+        short_name: 'Comiya',
+        description: 'A simple comic site',
+        theme_color: '#fff',
+        icons: [
+          {
+            src: 'comiya.svg',
+            sizes: '48x48',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'comiya.svg',
+            sizes: '96x96',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'comiya.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'comiya.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'comiya.svg',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'comiya.svg',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        display: 'standalone',
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
